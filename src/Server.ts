da406104ce57420f2b81584f0c33a91830a00a1f
vishalnirwan1 <vishal@successive.tech>
2019-07-30 18:30:49 +0530

    add not found message

--- src/Server.ts
+++ src/Server.ts
@@ -1,7 +1,7 @@
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
-// import { middle2 } from './libs/routes/errorHandler';
-// import { middle1 } from './libs/routes/notFoundRoute';
+import { middle2 } from './libs/routes/errorHandler';
+import { middle1 } from './libs/routes/notFoundRoute';
 import router from './router';
 
 const app = express();
@@ -21,8 +21,8 @@ export default class Server {
             res.send( 'I am Ok' );
        });
         app.use('/api', router);
-        // app.use(middle1);
-        // app.use(middle2);
+        app.use(middle1);
+        app.use(middle2);
         return this;
     }
        public run() {
--- src/libs/routes/errorHandler.ts
+++ src/libs/routes/errorHandler.ts
@@ -2,7 +2,7 @@ export const middle2 = (err, req, res, next) => {
     res.send({
         error: 'Not Found',
         message: err,
-        status: 500,
+        status: 404,
         timestamp: new Date(),
     });
 };
