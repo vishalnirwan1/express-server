0f03d95d2f15f453692db6861e31a1e7a48376a2
vishalnirwan1 <vishal@successive.tech>
2019-08-08 10:55:11 +0530

    changes in seedData

--- src/libs/seedData.ts
+++ src/libs/seedData.ts
@@ -1,38 +1,38 @@
 import UserRepository from '../repositories/user/UserRepository';
 
 const userRepository = new UserRepository();
-export default() => {
+export default () => {
     console.log('inside seed data');
     const user = {
         name: 'Vishal',
         email: 'vishal@gmail.com',
     };
     userRepository.create(user)
-    .then((res) => {
-        console.log('user created ', res);
-    })
-    .catch((err) => {
-        console.log('error issss', err);
-    });
-    userRepository.update({name: 'Vishal'}, {name: 'Nirwan', email: 'nirwan@gmail.com'})
-    .then((res) => {
-        console.log('user updated ', res);
-    })
-    .catch((err) => {
-        console.log('error issss', err);
-    });
-    userRepository.get({name: 'Nirwan'}, undefined, undefined)
-    .then((res) => {
-        console.log('user got ', res);
-    })
-    .catch((err) => {
-        console.log('error issss', err);
-    });
-    userRepository.delete({name: 'Vishal'})
-    .then((res) => {
-        console.log('user deleted ', res);
-    })
-    .catch((err) => {
-        console.log('error issss', err);
-    });
+        .then((res) => {
+            console.log('user created --', res);
+            userRepository.update({ name: 'Vishal' }, { name: 'Nirwan', email: 'nirwan@gmail.com' })
+                .then((res) => {
+                    console.log('user updated --', res);
+                })
+                .catch((err) => {
+                    console.log('error issss', err);
+                });
+            userRepository.get({ name: 'Nirwan' }, undefined, undefined)
+                .then((res) => {
+                    console.log('user fetched --', res);
+                })
+                .catch((err) => {
+                    console.log('error issss', err);
+                });
+            userRepository.delete({ name: 'Vishal' })
+                .then((res) => {
+                    console.log('user deleted --', res);
+                })
+                .catch((err) => {
+                    console.log('error issss', err);
+                });
+        })
+        .catch((err) => {
+            console.log('error issss', err);
+        });
 };
